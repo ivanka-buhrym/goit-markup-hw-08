a371395f9a17c2552fc88994b2231682032c92fc
Ivanka Buhrym <ivankabuhrim@gmail.com>
2022-02-21 15:14:56 +0200

    Added adapted verions for 4 sections (portfolio, teams. clients and features)

--- js/modal.js
+++ js/modal.js
@@ -5,10 +5,10 @@
       modal: document.querySelector("[data-modal]"),
     };
   
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
   })();
