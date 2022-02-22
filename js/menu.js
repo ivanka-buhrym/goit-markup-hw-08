ff26c5663bead4c705be530d0d94e40afaab2bf4
Ivanka Buhrym <ivankabuhrim@gmail.com>
2022-02-22 10:00:53 +0200

    added button

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,11 @@
     const refs = {
       openMenuBtn: document.querySelector("[mob-menu-open]"),
       closeMenuBtn: document.querySelector("[mob-menu-close]"),
-      Menu: document.querySelector("[mob-menu]"),
+      menu: document.querySelector("[mob-menu]"),
     };
   
     function toggleMenu() {
-      refs.modal.classList.toggle("is-hidden");
+      refs.menu.classList.toggle("is-hidden");
     }
     refs.openMenuBtn.addEventListener("click", toggleMenu);
     refs.closeMenuBtn.addEventListener("click", toggleMenu);
